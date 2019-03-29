a74c357c44129720e89d39052a33eba5a12c5e41
swathi <swathi261992@gmail.com>
2019-03-29 15:27:49 +0530

    test

--- Gitdemonewtest/TestViewController.swift
+++ Gitdemonewtest/TestViewController.swift
@@ -6,28 +6,28 @@
 //  Copyright © 2018 test. All rights reserved.
 //
 
-import UIKit
-
-class TestViewController: UIViewController {
-
-    @IBOutlet weak var testlbl: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let instanceOfCustomObject: customobjc = customobjc()
-        instanceOfCustomObject.someProperty = "Hello World"
-        print(instanceOfCustomObject.someProperty)
-        instanceOfCustomObject.someMethod()
-        testlbl.text = "Test";
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-   
+//import UIKit
+//
+//class TestViewController: UIViewController {
+//
+//    @IBOutlet weak var testlbl: UILabel!
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        let instanceOfCustomObject: customobjc = customobjc()
+//        instanceOfCustomObject.someProperty = "Hello World"
+//        print(instanceOfCustomObject.someProperty)
+//        instanceOfCustomObject.someMethod()
+//        testlbl.text = "Test";
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//
+//   
     
 
     /*
--- Gitdemonewtest/swiftClass.swift
+++ Gitdemonewtest/swiftClass.swift
@@ -6,17 +6,17 @@
 //  Copyright © 2018 test. All rights reserved.
 //
 
-import UIKit
-
-@objc class swiftClass: NSObject {
-    var someProperty: AnyObject = "Some Initializer Val" as AnyObject
-    override init() {}
-   
-   @objc func someFunction(someArg:String) -> String {
-        let returnVal = "You sent me \(someArg)"
-        return returnVal
-    
-    }
-    
-
-}
+//import UIKit
+//
+//@objc class swiftClass: NSObject {
+//    var someProperty: AnyObject = "Some Initializer Val" as AnyObject
+//    override init() {}
+//   
+//   @objc func someFunction(someArg:String) -> String {
+//        let returnVal = "You sent me \(someArg)"
+//        return returnVal
+//    
+//    }
+//    
+//
+//}
